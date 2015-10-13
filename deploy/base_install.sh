944b1ea633d35117f45a2721e9e54ff5dd6431ac
wangxin <wangxin@node7.(none)>
2015-10-13 20:10:53 +0800

    auto deploy system

---
+++ deploy/base_install.sh
@@ -0,0 +1,52 @@
+#########################################################################
+# File Name: base_install.sh
+# Author: wangxin
+# mail: wangxin4@iie.ac.cn
+# Created Time: Mon 12 Oct 2015 10:49:07 AM CST
+#########################################################################
+#!/bin/bash
+
+script_dir=`dirname $BASH_SOURCE`
+echo $script_dir
+
+tcl_version=8.5.18
+tcl_tar_name=tcl8.5.18-src.tar.gz
+tcl_tar_path=$script_dir/depend_ware/$tcl_tar_name
+
+expect_version=5.45
+expect_tar_name=expect5.45.tar.gz
+expect_tar_path=$script_dir/depend_ware/$expect_tar_name
+
+
+
+#install tcl
+tar zxvf $tcl_tar_path
+tcl_tag=`ls -l /usr/local/bin/ /usr/bin/|grep tcl|wc -l`
+if [ $tcl_tag == 0 ]
+then
+    cd $script_dir/tcl$tcl_version/unix
+    ./configure
+    make
+    make install
+    cd -
+else
+    echo 'tcl alread installed'
+fi
+
+#install expect
+expect_tag=`ls -l /usr/local/bin/ /usr/bin/|grep tcl|wc -l`
+if [ $expect_tag == 0 ]
+then
+    tar zxvf $expect_tar_path
+    cd  $script_dir/expect$expect_version
+    ./configure --with-tclinclude=$script_dir/tcl$tcl_version/generic/ --with-tclconfig=/usr/local/lib/
+    make
+    make install
+    cd -
+else
+    echo 'expect alread installed'
+fi
+
+rm -fr $script_dir/tcl$tcl_version
+rm -fr $script_dir/expect$expect_version
+
---
+++ deploy/ex_install.sh
@@ -0,0 +1,42 @@
+#########################################################################
+# File Name: ex_install.sh
+# Author: wangxin
+# mail: wangxin4@iie.ac.cn
+# Created Time: Mon 12 Oct 2015 11:52:45 AM CST
+#########################################################################
+#!/bin/bash
+
+#usage: ex_install.sh dest_host user_name
+#
+if [ $# != 2 ]
+then
+    echo "invalid parameter numer $#"
+    echo "usage: ex_install.sh dest_host user_name"
+    exit 1
+fi
+
+dst_host=$1
+usr_name=$2
+script_dir=`dirname $BASH_SOURCE`
+install_dir='/home/wangxin/panda_new/install'
+
+zmq_version=3.2.3
+zmq_tar_name=zeromq-3.2.3.tar.gz
+zmq_tar_path=$script_dir/depend_ware/$zmq_tar_name
+
+
+ssh $usr_name@$dst_host "mkdir -p $install_dir"
+
+#install zmq
+zmq_tag=`ssh $usr_name@$dst_host 'ls -l /usr/local/lib*|grep zmq|wc -l'`
+if [ $zmq_tag == 0 ]
+then
+    scp $zmq_tar_path  $usr_name@$dst_host:$install_dir
+    ssh $usr_name@$dst_host "cd $install_dir && tar zxvf $install_dir/$zmq_tar_name&& cd zeromq-$zmq_version&& ./configure&& make&& make install && make clean && cd $install_dir && rm -fr $install_dir/zeromq-$zmq_version"
+else
+    echo "zmq alread installed on $host"
+fi
+
+echo "install complete"
+
+
